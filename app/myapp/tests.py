6f73a54762649b449dc7bacd06a209e31586420e
Pradeep <pradeep_c@rollingarrays.com>
2021-10-16 12:17:49 +0530

    ADDED: Calc functions & test cases

--- app/myapp/tests.py
+++ app/myapp/tests.py
@@ -1,3 +1,16 @@
-# from django.test import TestCase
+from django.test import TestCase
 
-# # Create your tests here.
+from myapp.utils import add, subtract
+
+# Create your tests here.
+
+
+class CalcTests(TestCase):
+
+    def test_add_numbers(self):
+        """ Test to add numbers """
+        self.assertEqual(add(1, 5), 6)
+
+    def test_sub_numbers(self):
+        """ Test to subtract numbers """
+        self.assertEqual(subtract(5, 11), 6)
