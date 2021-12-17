24fba70f7938981732fbd9e8c6dddd223b207124
Pradeep <pradeep_c@rollingarrays.com>
2021-12-17 12:49:52 +0530

    ADDED: Postgres Config

--- app/myapp/tests.py
+++ app/myapp/tests.py
@@ -1,16 +1,25 @@
+from unittest.mock import patch
+
+from django.core.management import call_command
+from django.db.utils import OperationalError
 from django.test import TestCase
 
-from myapp.utils import add, subtract
-
-# Create your tests here.
+# from myapp.management.commands import wait_for_db
 
 
-class CalcTests(TestCase):
+class CommandTests(TestCase):
 
-    def test_add_numbers(self):
-        """ Test to add numbers """
-        self.assertEqual(add(1, 5), 6)
+    def test_wait_for_db_ready(self):
+        """ Test waiting for db when db is available """
+        with patch('django.db.utils.ConnectionHandler.__getitem__') as getitem:
+            getitem.return_value = True
+            call_command('wait_for_db')
+            self.assertEqual(getitem.call_count, 1)
 
-    def test_sub_numbers(self):
-        """ Test to subtract numbers """
-        self.assertEqual(subtract(5, 11), 6)
+    @patch('time.sleep', return_value=True)
+    def test_wait_for_db(self, ts):
+        """ Test waiting for db."""
+        with patch('django.db.utils.ConnectionHandler.__getitem__') as getitem:
+            getitem.side_effect = [OperationalError] * 5 + [True]
+            call_command('wait_for_db')
+            self.assertEqual(getitem.call_count, 6)
